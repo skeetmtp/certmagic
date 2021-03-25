4781b15046057a80721a403c3440a0ef7e06786b
Alban Lecocq <alban@lempire.co>
2021-03-25 09:07:47 +0100

    Disable rate limit by default

--- acmeclient.go
+++ acmeclient.go
@@ -329,7 +329,7 @@ var (
 
 	// RateLimitEventsWindow is the size of the sliding
 	// window that throttles events.
-	RateLimitEventsWindow = 1 * time.Minute
+	RateLimitEventsWindow = 0 * time.Minute
 )
 
 // Some default values passed down to the underlying ACME client.
